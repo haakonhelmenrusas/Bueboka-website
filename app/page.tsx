61b365772b5856fbde275fe4b489e19c9102e2ee
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2024-06-07 15:49:37 +0200

    feat: header wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,61 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<div className={styles.header}>
-				<h1 className={styles.title}>Velkommen til Bueboka</h1>
-				<Image className={styles.logo} src='/next.svg' alt='Next.js Logo' width={180} height={37} priority />
-			</div>
-		</main>
-	);
+  return (
+    <main className={styles.main}>
+      <section className={styles.header}>
+        <div className={styles.header_info}>
+          <h1 className={styles.title}>Bueboka</h1>
+          <p className={styles.description}>Bueskyting for alle </p>
+          <div className={styles.storeLinks}>
+            <a
+              href="https://play.google.com/store/apps/details?id=com.aaronshade.bueboka&hl=no_nb"
+              aria-label="Link to Play Store"
+              className={styles.button}
+            >
+              <Image
+                className={styles.logo}
+                src="/assets/playStore.png"
+                alt="Phone"
+                width={220}
+                height={64}
+                priority
+              />
+            </a>
+            <a
+              href="/"
+              aria-label="Link to App Store"
+              className={styles.button}
+            >
+              <Image
+                className={styles.logo}
+                src="/assets/appStore.png"
+                alt="Phone"
+                width={220}
+                height={64}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            className={styles.logo}
+            src="/assets/iOS.png"
+            alt="Phone"
+            width={300}
+            height={600}
+          />
+          <Image
+            className={styles.logo}
+            src="/assets/Android.png"
+            alt="Mobile"
+            width={300}
+            height={600}
+          />
+        </div>
+      </section>
+    </main>
+  );
 }
