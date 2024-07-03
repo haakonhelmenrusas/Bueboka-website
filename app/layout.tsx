2d7a07361394b0c5d8019030d938ca159e70764d
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2024-07-03 09:20:58 +0200

    feat: meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,39 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'Bueboka',
-	description: 'En ny app for bueskyttere i Norge',
+  title: "Bueboka",
+  description: "En ny app for bueskyttere i Norge",
+  applicationName: "Bueboka",
+  authors: [
+    {
+      name: "Haakon Helmen Rusås",
+      url: "https://github.com/haakonhelmenrusas",
+    },
+  ],
+  keywords: [
+    "bueskyting",
+    "bueboka",
+    "idrett",
+    "trening",
+    "konkurranse",
+    "app",
+    "norge",
+    "ios",
+    "android",
+  ],
+  publisher: "Rusås Design",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
