cd06f8dcb0647d9a32e7a6f369c1d0f17915b2bc
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-09-06 15:36:02 +0200

    ci: Setup singleton instance of Prisma client
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import {withAccelerate} from '@prisma/extension-accelerate'
+import {PrismaClient} from '@prisma/client/edge';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
+    globalForPrisma.prisma ||
     new PrismaClient({
-        log: ["query", "error"],
-    });
+        log: ['error', 'warn'],
+    }).$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
