5dd89aca997633f24180f37d956e227cec4e048f
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-12-22 16:02:16 +0100

    fix: update prisma client
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,20 @@
-import {withAccelerate} from '@prisma/extension-accelerate'
-import {PrismaClient} from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from '@/prisma/prisma/generated/prisma-client/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient;
+};
 
 export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['error', 'warn'],
-    }).$extends(withAccelerate());
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		adapter,
+	}).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
