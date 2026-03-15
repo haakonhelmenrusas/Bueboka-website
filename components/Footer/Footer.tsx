4ff1eef234da7c3aa10768454321145d82ed483e
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2026-03-15 13:41:48 +0100

    feat: redesign footer to include sponsors section and update call-to-action text
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { LuGithub } from 'react-icons/lu';
+import { LuExternalLink } from 'react-icons/lu';
 import { AppStoreBadge } from '@/components';
 import React from 'react';
 import styles from './Footer.module.css';
@@ -15,23 +15,43 @@ export function Footer() {
 						</div>
 						<span className={styles.brandText}>Bueboka</span>
 					</div>
+
 					<p className={styles.lead}>
-						Klar til å ta bueskyting-ferdighetene dine til neste nivå? Sjekk ut koden vår på GitHub eller last ned appen i dag.
+						Last ned Bueboka-appen og ta treningsdagboken med deg overalt.
 					</p>
-					<div className={styles.ctaRow}>
-						<a
-							href="https://github.com/haakonhelmenrusas/Bueboka-website/"
-							target="_blank"
-							className={`${styles.ctaBtn} ${styles.pressable}`}
-						>
-							<LuGithub className={styles.icon} />
-							<span>GitHub</span>
-						</a>
-					</div>
 					<div className={styles.badgesRow}>
 						<AppStoreBadge store="ios" href="https://apps.apple.com/app/bueboka" />
 						<AppStoreBadge store="android" href="https://play.google.com/store/apps/details?id=com.bueboka" />
 					</div>
+
+					<div className={styles.sponsorsSection}>
+						<p className={styles.sponsorsLabel}>Samarbeidspartnere</p>
+						<div className={styles.sponsorsGrid}>
+							<div className={styles.sponsorCard}>
+								<div className={styles.sponsorLogoWrap}>
+									<Image
+										src="/assets/arcticBueLogo.png"
+										alt="Arctic bueskyting logo"
+										width={120}
+										height={96}
+										className={styles.sponsorLogo}
+									/>
+								</div>
+								<h3 className={styles.sponsorName}>Arctic bueskyting</h3>
+								<p className={styles.sponsorDesc}>Premium utstyr for alle bueskyttere</p>
+								<a
+									href="https://www.arcticbuesport.no"
+									target="_blank"
+									rel="noopener noreferrer"
+									className={styles.sponsorLink}
+								>
+									Besøk nettside
+									<LuExternalLink size={14} />
+								</a>
+							</div>
+						</div>
+					</div>
+
 					<div className={styles.separator}>
 						<p className={styles.finePrint}>
 							© 2026 Bueboka. Alle rettigheter forbeholdt. Laget med ❤️ av{' '}
